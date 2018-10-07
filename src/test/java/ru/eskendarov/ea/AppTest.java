97c6930d142c3f7810f45cccc21fd7161a4e4f99
EnverGeekBrains <41239252+EnverGeekBrains@users.noreply.github.com>
2018-10-07 18:04:21 +0300

    Add files via upload

---
+++ src/test/java/ru/eskendarov/ea/AppTest.java
@@ -0,0 +1,19 @@
+package ru.eskendarov.ea;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public static void main(String[] args) {
+    }
+}
